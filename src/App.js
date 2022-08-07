cee6316e643d8cf711c553c31b72fdb0b6d962f9
Tony Jemba <jjembatony@gmail.com>
2022-08-07 07:32:07 +0300

    Starter template

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link bg-red-800"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+     
     </div>
   );
 }
